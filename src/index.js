13ae81981b0eabc54da85051d6fa19e40eb220bb
Irina Shasheva <loyrina@gmail.com>
2019-05-03 01:18:47 +0300

    fix: remove props with time and errors count

--- src/index.js
+++ src/index.js
@@ -5,17 +5,10 @@ import App from './components/app/app.jsx';
 
 /** Entry point for project */
 const init = () => {
-  const settings = {
-    gameTime: 5,
-    errorCount: 3,
-  };
 
   /** React render for App component */
   ReactDOM.render(
-      <App
-        gameTime={settings.gameTime}
-        errorCount={settings.errorCount}
-      />,
+      <App/>,
       document.querySelector(`.main`)
   );
 };

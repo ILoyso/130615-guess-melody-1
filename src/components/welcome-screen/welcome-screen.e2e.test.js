3c4061ad2c67342d6e6a7870250839a40c4a88a5
Irina Shasheva <loyrina@gmail.com>
2019-05-08 11:03:40 +0300

    refactor: replace quotes from "" to '' in e2e test for WelcomeScreen

--- src/components/welcome-screen/welcome-screen.e2e.test.js
+++ src/components/welcome-screen/welcome-screen.e2e.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import Enzyme, {shallow} from "enzyme";
-import Adapter from "enzyme-adapter-react-16";
+import Enzyme, {shallow} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 
 import WelcomeScreen from './welcome-screen';
 

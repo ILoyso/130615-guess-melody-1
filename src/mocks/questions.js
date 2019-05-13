41cd066b87eea096bb96c9593d10205bbe831248
Irina Shasheva <loyrina@gmail.com>
2019-05-13 21:54:23 +0300

    frat: add questions mock

---
+++ src/mocks/questions.js
@@ -0,0 +1,45 @@
+export const questions = [
+  {
+    answers: [
+      {
+        src: `https://upload.wikimedia.org/wikipedia/commons/1/1f/Uganda_flag_and_national_anthem_-_Oh_Uganda_Land_o.ogg`,
+        genre: `rock`,
+      },
+      {
+        src: `https://upload.wikimedia.org/wikipedia/commons/1/1f/Uganda_flag_and_national_anthem_-_Oh_Uganda_Land_o.ogg`,
+        genre: `blues`,
+      },
+      {
+        src: `https://upload.wikimedia.org/wikipedia/commons/1/1f/Uganda_flag_and_national_anthem_-_Oh_Uganda_Land_o.ogg`,
+        genre: `jazz`,
+      },
+      {
+        src: `https://upload.wikimedia.org/wikipedia/commons/1/1f/Uganda_flag_and_national_anthem_-_Oh_Uganda_Land_o.ogg`,
+        genre: `rock`,
+      },
+    ],
+    genre: `rock`,
+    type: `genre`,
+  },
+  {
+    answers: [
+      {
+        picture: `http://placehold.it/134x134`,
+        artist: `Jared Leto`,
+      },
+      {
+        picture: `http://placehold.it/134x134`,
+        artist: `Dave Grohl`,
+      },
+      {
+        picture: `http://placehold.it/134x134`,
+        artist: `Kurt Cobain`,
+      },
+    ],
+    song: {
+      artist: `Kurt Cobain`,
+      src: `path.mp3`,
+    },
+    type: `artist`,
+  },
+];

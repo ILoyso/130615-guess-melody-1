7d80d83adc51185ea9becc363f20bc1deecc53dd
Irina Shasheva <loyrina@gmail.com>
2019-05-25 16:33:22 +0300

    feat: add tests for functions witch check user unswers

--- src/reducer.test.js
+++ src/reducer.test.js
@@ -1,93 +1,165 @@
-import {reducer} from './reducer';
+import {
+  isArtistAnswerCorrect,
+  isGenreAnswerCorrect,
+  reducer
+} from './reducer';
 
-describe(`Reducer`, () => {
+describe(`Business logic is correct`, () => {
+  it(`Artist answer is checked correctly`, () => {
+    expect(isArtistAnswerCorrect({
+      artist: `correct-artist`,
+      picture: `correct-pic`,
+    }, {
+      type: `artist`,
+      song: {
+        artist: `correct-artist`,
+        src: ``,
+      },
+      answers: [
+        {
+          artist: `incorrect-artist`,
+          picture: `incorrect-pic`,
+        },
+        {
+          artist: `correct-artist`,
+          picture: `correct-pic`,
+        },
+        {
+          artist: `incorrect-artist-2`,
+          picture: `incorrect-pic`,
+        },
+      ]
+    })).toBe(true);
+
+    expect(isArtistAnswerCorrect({
+      artist: `incorrect-artist-2`,
+      picture: `incorrect-pic`,
+    }, {
+      type: `artist`,
+      song: {
+        artist: `correct-artist`,
+        src: ``,
+      },
+      answers: [
+        {
+          artist: `incorrect-artist`,
+          picture: `incorrect-pic`,
+        },
+        {
+          artist: `correct-artist`,
+          picture: `correct-pic`,
+        },
+        {
+          artist: `incorrect-artist-2`,
+          picture: `incorrect-pic`,
+        },
+      ]
+    })).toBe(false);
+  });
+
+  it(`Genre question is checked correctly`, () => {
+    expect(isGenreAnswerCorrect([false, false, true, false], {
+      type: `genre`,
+      genre: `rock`,
+      answers: [
+        {
+          genre: `jazz`,
+          src: `0`,
+        },
+        {
+          genre: `blues`,
+          src: `1`,
+        },
+        {
+          genre: `rock`,
+          src: `2`,
+        },
+        {
+          genre: `jazz`,
+          src: `3`,
+        },
+      ]
+    })).toEqual(true);
+
+
+    expect(isGenreAnswerCorrect([false, false, false, false], {
+      type: `genre`,
+      genre: `jazz`,
+      answers: [
+        {
+          genre: `jazz`,
+          src: `0`,
+        },
+        {
+          genre: `jazz`,
+          src: `1`,
+        },
+        {
+          genre: `rock`,
+          src: `2`,
+        },
+        {
+          genre: `blues`,
+          src: `3`,
+        },
+      ]
+    })).toEqual(false);
+  });
+});
+
+describe(`Reducer works correctly`, () => {
   it(`Reducer without additional parameters should return initial state`, () => {
     expect(reducer(undefined, {})).toEqual({
-      mistakes: 0,
       step: -1,
+      mistakes: 0,
     });
   });
 
-  it(`Reduser should increment step by a given value`, () => {
+  it(`Reducer should increment current step by a given value`, () => {
     expect(reducer({
+      step: -1,
       mistakes: 0,
-      step: -1
     }, {
+      type: `INCREMENT_STEP`,
       payload: 1,
-      type: `INCREMENT_STEP`,
     })).toEqual({
+      step: 0,
       mistakes: 0,
-      step: 0
     });
 
     expect(reducer({
-      mistakes: 1,
-      step: 2
-    }, {
-      payload: -1,
-      type: `INCREMENT_STEP`,
-    })).toEqual({
-      mistakes: 1,
-      step: 1
-    });
-
-    expect(reducer({
+      step: -1,
       mistakes: 0,
-      step: 1
     }, {
+      type: `INCREMENT_STEP`,
       payload: 0,
-      type: `INCREMENT_STEP`,
     })).toEqual({
+      step: -1,
       mistakes: 0,
-      step: 1
     });
   });
 
-  it(`Reduser should increment mistakes by a given value`, () => {
+  it(`Reducer should increment number of mistakes by a given value`, () => {
     expect(reducer({
+      step: -1,
       mistakes: 0,
-      step: -1
     }, {
+      type: `INCREMENT_MISTAKES`,
       payload: 1,
-      type: `INCREMENT_MISTAKES`,
     })).toEqual({
+      step: -1,
       mistakes: 1,
-      step: -1
     });
 
     expect(reducer({
-      mistakes: 2,
-      step: 2
+      step: -1,
+      mistakes: 0,
     }, {
-      payload: -1,
       type: `INCREMENT_MISTAKES`,
-    })).toEqual({
-      mistakes: 1,
-      step: 2
-    });
-
-    expect(reducer({
-      mistakes: 1,
-      step: 1
-    }, {
       payload: 0,
-      type: `INCREMENT_MISTAKES`,
     })).toEqual({
-      mistakes: 1,
-      step: 1
-    });
-  });
-
-  it(`Reducer should reset game`, () => {
-    expect(reducer({
-      mistakes: 3,
-      step: 5
-    }, {
-      payload: 1,
-      type: `RESET_GAME`,
-    })).toEqual({
+      step: -1,
       mistakes: 0,
-      step: -1
     });
   });
 });
